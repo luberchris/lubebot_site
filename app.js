a087dee03b95ec861489d22a0feea9767860198a
cdrlu <cdrluber@gmail.com>
2019-05-20 12:17:09 -0400

    created react frontend

--- app.js
+++ app.js
@@ -1,165 +1,10 @@
-const tmi = require("tmi.js");
-const _ = require("lodash");
-require("dotenv").config();
+const helpers = require("./helpers");
+const controller = require("./controller");
 
 //////////////////Global Variables////////////////////
-
-let yesCount = 0;
-let noCount = 0;
+const client = controller.client;
 
 // const drink = new Audio("./assets/sounds/ice_cubes.mp3");
-
-const insults = [
-  "lil bitch",
-  "whatever dummy",
-  "fuggin uh",
-  "okay shut up",
-  "you waste of space"
-];
-
-//////////////////Helpers////////////////////
-const convertSeconds = seconds => {
-  let minutes = seconds / 60;
-  let remainder = seconds % 60;
-  return minutes + " minutes and " + remainder + " seconds";
-};
-
-const getRandomInt = max => {
-  return Math.floor(Math.random() * Math.floor(max));
-};
-
-const getVotes = channel => {
-  yesCount = 0;
-  noCount = 0;
-  client.action(
-    channel,
-    "30 seconds: #yes to make Luber take a shot, #no otherwise"
-  );
-  setTimeout(() => {
-    client.action(
-      channel,
-      "15 seconds left!!! #yes to make Luber take a shot!"
-    );
-  }, 15000);
-  var promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (yesCount > noCount || yesCount === noCount) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    }, 30000);
-  });
-  return promise;
-};
-
-//Props to unicorn_dev for the code and the tutoring - I'm drunk
-const gridPrint = (strang, channel, user) => {
-  const messageParts = strang.split(" ");
-  const messageLengthParts = messageParts.length;
-  let repeated = messageParts.slice(1, messageLengthParts - 2).join(" ");
-
-  if (messageLengthParts === 2) {
-    repeated = messageParts[1];
-  }
-
-  let width = messageParts[strang.split(" ").length - 2];
-  let height = messageParts[strang.split(" ").length - 1];
-
-  if ((messageLengthParts >= 3 && isNaN(height)) || messageLengthParts < 3) {
-    height = 1;
-  }
-
-  if ((messageLengthParts >= 4 && isNaN(width)) || messageLengthParts < 4) {
-    width = 1;
-  }
-
-  const emoteArray = [];
-
-  _.each(_.times(Number(width)), line => {
-    emoteArray.push(repeated);
-  });
-
-  console.log(emoteArray.join(" "));
-
-  _.each(_.times(Number(height)), line => {
-    client.say(removeHash(channel), emoteArray.join(" "));
-  });
-};
-
-const mock = message => {
-  let newString = "";
-  for (let i = 0; i < message.length; i++) {
-    if (i % 2 === 0) {
-      newString += message[i].toLowerCase();
-    } else {
-      newString += message[i].toUpperCase();
-    }
-  }
-  return newString;
-};
-
-const removeHash = user => {
-  if (user.charAt(0) === "#") {
-    return (
-      user
-        .substr(1, user.length)
-        .charAt(0)
-        .toUpperCase() + user.substr(2, user.length)
-    );
-  } else {
-    return user.charAt(0).toUpperCase() + user.slice(1);
-  }
-};
-
-//////////////////Twitch Connection Options////////////////////
-const options = {
-  options: {
-    debug: true
-  },
-  connection: {
-    cluster: "aws",
-    reconnect: true
-  },
-  identity: {
-    username: "lubechatbot",
-    password: process.env.TTV_OAUTH
-  },
-  channels: ["Lubesy"]
-};
-
-const client = new tmi.client(options);
-
-//////////////////Connection Actions////////////////////
-client.connect();
-
-//Connecting
-client.on("connecting", (address, port) => {
-  console.log("Attempting to connect to Twitch: " + address + ":" + port);
-});
-
-//Connected
-client.on("connected", (address, port) => {
-  console.log("Connected to Twitch: " + address + ":" + port);
-});
-
-//Disconnected
-client.on("disconnected", reason => {
-  console.log(
-    `Disconnected\n
-  ----------------\n` + reason
-  );
-});
-
-//Reconnect
-client.on("reconnect", () => {
-  console.log("Reconnected to Twitch");
-});
-
-//Serverchange
-client.on("serverchange", channel => {
-  console.log("Server has switched: " + removeHash(channel));
-});
 
 //////////////////Chat Actions////////////////////
 
@@ -169,17 +14,17 @@ client.on("serverchange", channel => {
 //Ban
 client.on("ban", (channel, username, reason) => {
   client.action(
-    removeHash(channel),
+    helpers.removeHash(channel),
     "LUL LUL LUL " +
-      removeHash(username) +
+      helpers.removeHash(username) +
       " was banned from " +
-      removeHash(channel) +
+      helpers.removeHash(channel) +
       "'s chat for " +
       reason
   );
   client.whisper(
     user.username,
-    "You were banned from " + removeHash(channel) + " for " + reason
+    "You were banned from " + helpers.removeHash(channel) + " for " + reason
   );
 });
 
@@ -188,42 +33,42 @@ client.on("chat", (channel, user, message, self) => {
   switch (message.split(" ").length) {
     case 1:
       switch (message) {
-        case "!damnitgary":
-          client.action(removeHash(channel), "God fucking dammit");
+        case "!dammitgary":
+          client.say(helpers.removeHash(channel), "G-d fucking dammit");
           break;
         case "!drink":
-          client.action(
-            removeHash(channel),
-            removeHash(channel) + " you better DRINK"
+          client.say(
+            helpers.removeHash(channel),
+            helpers.removeHash(channel) + " you better DRINK"
           );
           break;
         //revise
         case "!gaytest":
-          client.action(
-            removeHash(channel),
+          client.say(
+            helpers.removeHash(channel),
             "GayPride " +
               user.username +
               " is " +
-              getRandomInt(100) +
+              helpers.getRandomInt(100) +
               "% gay GayPride"
           );
           break;
         case "!lurk":
-          client.action(
-            removeHash(channel),
+          client.say(
+            helpers.removeHash(channel),
             user.username +
               ", thank you for the lurk! Remember to mute the tab and not the stream"
           );
           break;
         case "!mods":
-          client.mods(removeHash(channel)).then(mods => {
-            client.action(removeHash(channel), "Mods: " + mods);
+          client.mods(helpers.removeHash(channel)).then(mods => {
+            client.action(helpers.removeHash(channel), "Mods: " + mods);
           });
           break;
         case "!shot":
-          getVotes(removeHash(channel)).then(done => {
+          helpers.getVotes(helpers.removeHash(channel)).then(done => {
             client.action(
-              removeHash(channel),
+              helpers.removeHash(channel),
               done
                 ? "Take a shot, honey"
                 : "You don't need to take a shot right now, honey"
@@ -232,28 +77,22 @@ client.on("chat", (channel, user, message, self) => {
           break;
         case "!stairs":
           //Add soundbyte of Eddy
-          client.action(
-            removeHash(channel),
+          client.say(
+            helpers.removeHash(channel),
             "NotLikeThis NotLikeThis NotLikeThis WHAT HAPPENED TO THE STAIRS?? NotLikeThis NotLikeThis NotLikeThis "
           );
           break;
         case "!twitter":
-          client.action(
-            removeHash(channel),
+          client.say(
+            helpers.removeHash(channel),
             "Here's Chris's Twitter: https://twitter.com/luberchris"
           );
           break;
         case "!website":
-          client.action(
-            removeHash(channel),
+          client.say(
+            helpers.removeHash(channel),
             "Here's Chris's website: http://chrisluber.com"
           );
-          break;
-        case "#yes":
-          yesCount += 1;
-          break;
-        case "#no":
-          noCount += 1;
           break;
       }
 
@@ -262,14 +101,12 @@ client.on("chat", (channel, user, message, self) => {
         //Chat commands
         case "!mock":
           client.action(
-            removeHash(channel),
-            mock(message.substr(message.indexOf(" ") + 1)) +
-              ", " +
-              insults[getRandomInt(insults.length)]
+            helpers.removeHash(channel),
+            helpers.mock(message.substr(message.indexOf(" ") + 1))
           );
           break;
         case "!grid":
-          gridPrint(message, channel, user);
+          helpers.gridPrint(message, channel, user);
           break;
       }
   }
@@ -280,8 +117,8 @@ client.on("chat", (channel, user, message, self) => {
 //Clearchat
 client.on("clearchat", channel => {
   client.action(
-    removeHash(channel),
-    removeHash(channel) + "'s chat was cleared!"
+    helpers.removeHash(channel),
+    helpers.removeHash(channel) + "'s chat was cleared!"
   );
 });
 
@@ -294,13 +131,16 @@ client.on("clearchat", channel => {
 client.on("hosted", (channel, username, viewers, autohost) => {
   if (!autohost) {
     client.action(
-      removeHash(channel),
-      removeHash(username) + " is hosting us for " + viewers + " people!"
+      helpers.removeHash(channel),
+      helpers.removeHash(username) +
+        " is hosting us for " +
+        viewers +
+        " people!"
     );
     for (let i = 0; i < 10; i++) {
       client.action(
-        removeHash(channel),
-        removeHash(username) +
+        helpers.removeHash(channel),
+        helpers.removeHash(username) +
           " is hosting us for " +
           viewers +
           " people!\nGayPride GayPride GayPride GayPride GRAB THE LUBE BOYS GayPride GayPride GayPride GayPride "
@@ -308,7 +148,10 @@ client.on("hosted", (channel, username, viewers, autohost) => {
     }
   } else {
     console.log(
-      removeHash(username) + " is autohosting us for " + viewers + " people!"
+      helpers.removeHash(username) +
+        " is autohosting us for " +
+        viewers +
+        " people!"
     );
   }
 });
@@ -316,8 +159,8 @@ client.on("hosted", (channel, username, viewers, autohost) => {
 //Hosting
 client.on("hosting", (channel, target, viewers) => {
   client.action(
-    removeHash(channel),
-    viewers + " of us are hosting " + removeHash(target) + "!"
+    helpers.removeHash(channel),
+    viewers + " of us are hosting " + helpers.removeHash(target) + "!"
   );
 });
 
@@ -337,7 +180,7 @@ client.on("hosting", (channel, target, viewers) => {
 client.on("raided", (channel, username, viewers) => {
   for (let i = 0; i < 10; i++) {
     client.action(
-      removeHash(channel),
+      helpers.removeHash(channel),
       viewers +
         " NotLikeThis HeyGuys Oh golly here they come " +
         viewers +
@@ -345,10 +188,10 @@ client.on("raided", (channel, username, viewers) => {
     );
   }
   client.action(
-    removeHash(channel),
+    helpers.removeHash(channel),
     `My butt is ready GayPride GayPride GayPride GayPride GayPride GayPride GayPride GayPride\n
     We're being raided by ` +
-      removeHash(username) +
+      helpers.removeHash(username) +
       `and their ` +
       viewers +
       ` raiders!`
@@ -367,15 +210,17 @@ client.on("raided", (channel, username, viewers) => {
 //Timeout
 client.on("timeout", (channel, username, length, reason) => {
   client.action(
-    removeHash(channel),
-    removeHash(username) + " was shushed for " + convertSeconds(length)
+    helpers.removeHash(channel),
+    helpers.removeHash(username) +
+      " was shushed for " +
+      helpers.convertSeconds(length)
   );
   client.whisper(
-    removeHash(username),
+    helpers.removeHash(username),
     "You were timed out from " +
-      removeHash(channel) +
+      helpers.removeHash(channel) +
       "'s chat for " +
-      convertSeconds(length) +
+      helpers.convertSeconds(length) +
       " for " +
       reason
   );
---
+++ helpers.js
@@ -0,0 +1,116 @@
+const _ = require("lodash");
+const controller = require("./controller");
+
+// Import channel client
+const client = controller.client;
+
+// Keep track of votes
+let yesCount = 0;
+let noCount = 0;
+client.on("chat", (channel, user, message, self) => {
+  switch (message) {
+    case "#yes":
+      yesCount += 1;
+      break;
+    case "#no":
+      noCount += 1;
+      break;
+  }
+});
+
+module.exports = {
+  convertSeconds: function(seconds) {
+    let minutes = seconds / 60;
+    let remainder = seconds % 60;
+    return minutes + " minutes and " + remainder + " seconds";
+  },
+
+  getRandomInt: function(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  },
+
+  getVotes: function(channel) {
+    yesCount = 0;
+    noCount = 0;
+    client.action(
+      channel,
+      "30 seconds: #yes to make Luber take a shot, #no otherwise"
+    );
+    // setTimeout(() => {
+    //   client.action(
+    //     channel,
+    //     "15 seconds left!!! #yes to make Luber take a shot!"
+    //   );
+    // }, 15000);
+    var promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        client.say(channel, yesCount + " yes :" + noCount + " no");
+        if (yesCount > noCount || yesCount === noCount) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      }, 4000);
+    });
+    return promise;
+  },
+
+  //Props to unicorn_dev for the code and the tutoring - I'm drunk
+  gridPrint: function(strang, channel, user) {
+    const messageParts = strang.split(" ");
+    const messageLengthParts = messageParts.length;
+    let repeated = messageParts.slice(1, messageLengthParts - 2).join(" ");
+
+    if (messageLengthParts === 2) {
+      repeated = messageParts[1];
+    }
+
+    let width = messageParts[strang.split(" ").length - 2];
+    let height = messageParts[strang.split(" ").length - 1];
+
+    if ((messageLengthParts >= 3 && isNaN(height)) || messageLengthParts < 3) {
+      height = 1;
+    }
+
+    if ((messageLengthParts >= 4 && isNaN(width)) || messageLengthParts < 4) {
+      width = 1;
+    }
+
+    const emoteArray = [];
+
+    _.each(_.times(Number(width)), line => {
+      emoteArray.push(repeated);
+    });
+
+    console.log(emoteArray.join(" "));
+
+    _.each(_.times(Number(height)), line => {
+      client.say(this.removeHash(channel), emoteArray.join(" "));
+    });
+  },
+
+  mock: function(message) {
+    let newString = "";
+    for (let i = 0; i < message.length; i++) {
+      if (i % 2 === 0) {
+        newString += message[i].toLowerCase();
+      } else {
+        newString += message[i].toUpperCase();
+      }
+    }
+    return newString;
+  },
+
+  removeHash: function(user) {
+    if (user.charAt(0) === "#") {
+      return (
+        user
+          .substr(1, user.length)
+          .charAt(0)
+          .toUpperCase() + user.substr(2, user.length)
+      );
+    } else {
+      return user.charAt(0).toUpperCase() + user.slice(1);
+    }
+  }
+};
