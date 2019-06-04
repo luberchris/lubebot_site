cb0d5b847ccb3fbf3caccc729fd46f3062136437
cdrlu <cdrluber@gmail.com>
2019-06-04 11:36:11 -0400

    reset proxy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,16 +32,17 @@ class Home extends Component {
 
   getDataFromDb = () => {
     fetch("/api/getCounts")
-      .then(data => data.json())
-      .then(res => this.setState({ counts: res.data }));
+      .then(data => data.text())
+      .then(text => console.log(text));
+    // .then(res => this.setState({ counts: res.data }));
 
-    fetch("/api/getUsers")
-      .then(data => data.json())
-      .then(res => this.setState({ users: res.data }));
+    // fetch("/api/getUsers")
+    //   .then(data => data.json())
+    //   .then(res => this.setState({ users: res.data }));
 
-    fetch("/api/getEncounters")
-      .then(data => data.json())
-      .then(res => this.setState({ encounters: res.data }));
+    // fetch("/api/getEncounters")
+    //   .then(data => data.json())
+    //   .then(res => this.setState({ encounters: res.data }));
   };
 
   render() {
@@ -50,15 +51,23 @@ class Home extends Component {
       <div id="home-main">
         <span />
         <div id="navbar">
-          <a href="https://www.twitch.tv/keeelaan" target="_blank">Keeelaan</a>
-          <a href="https://www.twitch.tv/lubesy" target="_blank">Lubesy</a>
-          <a href="https://www.twitch.tv/drewbertdoo" target="_blank">DrewbertDoo</a>
-          <a href="https://www.twitch.tv/sethe_1" target="_blank">Sethe_1</a>
-          <a href="https://www.twitch.tv/lessthandennis" target="_blank">LessThanDennis</a>
+          <a href="https://www.twitch.tv/keeelaan" target="_blank">
+            Keeelaan
+          </a>
+          <a href="https://www.twitch.tv/lubesy" target="_blank">
+            Lubesy
+          </a>
+          <a href="https://www.twitch.tv/drewbertdoo" target="_blank">
+            DrewbertDoo
+          </a>
+          <a href="https://www.twitch.tv/sethe_1" target="_blank">
+            Sethe_1
+          </a>
+          <a href="https://www.twitch.tv/lessthandennis" target="_blank">
+            LessThanDennis
+          </a>
         </div>
-        <div class="jumbotron">
-          FAMILY TIME
-        </div>
+        <div class="jumbotron">FAMILY TIME</div>
         <div id="stats">
           <div>
             <h1>Counts:</h1>
